df78c80d0535682d87a979b3611b67baaccf44b8
Tecker.Yu <tecker_yuknigh@163.com>
2017-04-16 17:19:20 +0800

    Continue express framework configuration

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const path = require('path');
 const logger = require('morgan');
 const config = require('./config');
@@ -10,12 +13,22 @@ const api = require('./router/api');
 
 let app = express();
 
-app.use(logger('dev'))
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(cookieParser());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// Mount api before csrf is appended to the app stack 
 app.use('/api', api);
+
+app.use(logger('dev'))
+	.use(helmet())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+	.use(cookieParser())
+	.use(methodOverride())
+	.use(compression({filter: shouldCompress}))
+	.use(express.static(path.join(__dirname, 'public')));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,11 +48,19 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-if(!module.parent) {
-	app.listen(config.port, function(err) {
-		if(err) console.warn(err);
-		console.warn('Listening on http://localhost:' + config.port);
-	});
-}
 
-module.exports = app;
+app.listen(config.port, function(err) {
+	if(err) console.warn(err);
+	console.warn('Listening on http://localhost:' + config.port);
+});
+
+
+function shouldCompress(req, res) {
+	if (req.headers['x-no-compression']) {
+		// don't compress responses with this request header 
+		return false;
+	}
+
+	// fallback to standard filter function 
+	return compression.filter(req, res);
+}
--- server/router/api.js
+++ server/router/api.js
@@ -3,16 +3,18 @@ const router = express.Router();
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
+	console.log('Time: ', Date.now());
+	next();
 });
+
 // define the home page route
-router.get('/', (req, res) => {
-  res.send('Birds home page');
+router.get('/signup', (req, res) => {
+	res.send('signup');
 });
+
 // define the about route
-router.get('/about', (req, res) => {
-  res.send('About birds');
+router.post('/login', (req, res) => {
+
 });
 
 module.exports = router;
