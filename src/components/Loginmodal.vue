44882a0bf2bdd6766aa179c8a10b8a1f82d8c191
Tecker.Yu <tecker_yuknigh@163.com>
2017-04-20 22:55:10 +0800

    modify login template and data

--- src/components/Loginmodal.vue
+++ src/components/Loginmodal.vue
@@ -3,19 +3,20 @@
     <form class="form-horizontal">
       <fieldset>
         <legend>登录</legend>
-        <div class="form-group">
+        <div class="form-group" v-bind:class="{ 'has-error': hasError }">
           <label for="inputEmail" class="col-lg-2 control-label">邮箱</label>
           <div class="col-lg-10">
-            <input type="email" class="form-control" id="inputEmail">
+            <label v-if="hasError" class="control-label" for="focusedInput">{{ emailMessage }}</label>
+            <input v-model.trim="email" type="email" class="form-control input-lg" id="inputEmail">
           </div>
         </div>
         <div class="form-group">
           <label for="inputPassword" class="col-lg-2 control-label">密码</label>
           <div class="col-lg-10">
-            <input type="password" class="form-control" id="inputPassword">
+            <input v-model.trim="password" type="password" class="form-control input-lg" id="inputPassword">
             <div class="checkbox">
               <label>
-                <input type="checkbox"> 记住我
+                <input v-model="remember" type="checkbox"> 记住我
               </label>
             </div>
           </div>
@@ -36,7 +37,16 @@ export default {
   name: 'loginmodal',
   data () {
     return {
-
+      email: '',
+      password: '',
+      remember: false,
+      hasError: false,
+      emailMessage: '',
+      messages: {
+        default: '',
+        invalid: '请输入正确格式的邮箱地址',
+        incorrect: '用户名或密码错误，请重新输入'
+      }
     }
   }
 }
